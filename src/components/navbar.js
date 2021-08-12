b66cbde985619fc8c1f670a2910277a4c85716c4
ChrisKnowsDev <chrisharley81@gmail.com>
2021-08-12 09:11:28 -0400

    styled main navbar for desktop

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,7 @@ import { css, jsx } from '@emotion/react'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { IoMdBeer } from 'react-icons/io'
+import { FiMenu, FiX } from 'react-icons/fi'
 
 export default function Navbar() {
   const [open, setOpen] = useState(false)
@@ -15,22 +16,48 @@ export default function Navbar() {
         width: 100%;
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        max-width: var(--max-width);
+        margin: auto;
 
-        h3 {
+        .nav-icon {
+          display: none;
+          font-size: 2rem;
+          transition: var(--transition);
+        }
+
+        .logo {
+          color: var(--color-secondary);
           display: flex;
           align-items: center;
         }
 
-        .logo {
+        .logo-first-span {
           color: var(--color-light);
+        }
+
+        .logo-second-span {
+          color: var(--color-primary);
         }
 
         .logo-icon {
           color: var(--colorMid);
         }
 
+        .nav-links {
+          display: grid;
+          grid-template-columns: repeat(4, auto);
+          gap: 2rem;
+        }
+
         .nav-link {
           color: var(--color-light);
+          transition: var(--transition);
+
+          &:hover {
+            color: red;
+          }
         }
       `}
     >
@@ -38,12 +65,13 @@ export default function Navbar() {
       <h3>
         <Link to="/" className="logo">
           <IoMdBeer className="logo-icon" />
-          tapKeeper
+          <span className="logo-first-span">tap</span>
+          <span className="logo-second-span">Keeper</span>
         </Link>
       </h3>
 
       {/* navlinks */}
-      <ul className={open ? 'navlinks open' : 'navlinks'}>
+      <ul className={open ? 'nav-links open' : 'nav-links'}>
         <li className="nav-item">
           <Link to="/about" className="nav-link" onClick={() => setOpen(!open)}>
             About
